948f9879364887b096e3eb599484bd10a1a87326
Ravi Soni <ravisoni6262@gmail.com>
2022-04-27 16:12:29 +0530

    Shree Ganesh

---
+++ includes/class-connect-badgeos-to-discord-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.expresstechsoftwares.com
+ * @since      1.0.0
+ *
+ * @package    Connect_Badgeos_To_Discord
+ * @subpackage Connect_Badgeos_To_Discord/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Connect_Badgeos_To_Discord
+ * @subpackage Connect_Badgeos_To_Discord/includes
+ * @author     ExpressTech Softwares Solutions Pvt Ltd <contact@expresstechsoftwares.com>
+ */
+class Connect_Badgeos_To_Discord_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ public/class-connect-badgeos-to-discord-public.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://www.expresstechsoftwares.com
+ * @since      1.0.0
+ *
+ * @package    Connect_Badgeos_To_Discord
+ * @subpackage Connect_Badgeos_To_Discord/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    Connect_Badgeos_To_Discord
+ * @subpackage Connect_Badgeos_To_Discord/public
+ * @author     ExpressTech Softwares Solutions Pvt Ltd <contact@expresstechsoftwares.com>
+ */
+class Connect_Badgeos_To_Discord_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Connect_Badgeos_To_Discord_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Connect_Badgeos_To_Discord_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/connect-badgeos-to-discord-public.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Connect_Badgeos_To_Discord_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Connect_Badgeos_To_Discord_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/connect-badgeos-to-discord-public.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
